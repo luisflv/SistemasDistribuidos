92e8a76a9cf72b45821c911750158a02f95fa6ce
luisflv <luisflv@gmail.com>
2015-10-12 21:07:23 -0300

    Sockets with thread

---
+++ SocketComThread/ClientSocket/ProgramCliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ClientSocket
+{
+    class ProgramCliente
+    {
+        private static void Main(string[] args)
+        {
+            Thread thread = new Thread(() => ClienteSocket());
+            thread.Start();
+        }
+
+        public static void ClienteSocket()
+        {
+            // Cria o socket client
+            TcpClient client = new TcpClient();
+            // Conecta o socket Server
+            client.Connect("127.0.0.1", 2100);
+
+            // Obtem o Stream de comunicação
+            NetworkStream stream = client.GetStream();
+
+
+            // Envia mensagem para o Servidor
+            StreamWriter writer = new StreamWriter(stream);
+
+            Console.WriteLine("Digite um texto: ");
+            var msg = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            writer.WriteLine(msg);
+            writer.Flush();
+
+            // Recebe mensagem do Servidor
+            StreamReader reader = new StreamReader(stream);
+            var text = reader.ReadLine();
+
+            Console.WriteLine(text);
+            Console.ReadKey();
+        } 
+    }
+}
---
+++ SocketComThread/ServidorSocket/ProgramServidor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServidosSocket
+{
+    class ProgramServidor
+    {
+        private static string msg = "";
+        private static void Main(string[] args)
+        {
+            ServidorSocket();
+        }
+
+
+        private static void ThreadChat(TcpClient socket)
+        {
+            new Thread(() => StartWriteThread(socket)).Start();
+            new Thread(() => StartReaderThread(socket)).Start();
+
+        }
+
+        private static void StartReaderThread(TcpClient socket)
+        {
+            NetworkStream stream = socket.GetStream();
+
+            StreamReader reader = new StreamReader(stream);
+
+            while (msg != "exit")
+            {
+                msg = reader.ReadLine();
+                Console.WriteLine(msg);
+            }
+        }
+
+        private static void StartWriteThread(TcpClient socket)
+        {
+            NetworkStream stream = socket.GetStream();
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            Console.WriteLine("Digite um texto: ");
+            msg = string.Empty;
+
+            while (msg != "exit")
+            {
+                msg = Console.ReadLine();
+                writer.WriteLine(msg);
+                writer.Flush();
+            }
+        }
+
+        public static void ServidorSocket()
+        {
+            int port = 5000;
+            string ip = "172.16.2.80";
+
+            IPAddress ipAddress = IPAddress.Parse(ip);
+
+            //Cria o Socket Server
+            TcpListener server = new TcpListener(ipAddress, port);
+            //Inicia o Socket
+            server.Start();
+
+            while (true)
+            {
+               ThreadChat(server.AcceptTcpClient());
+            }
+        }
+    }
+}
